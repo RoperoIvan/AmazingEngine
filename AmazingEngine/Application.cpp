23293ba369188e1e73b44ccf402189f5ba7deaad
didaclis <36189205+didaclis@users.noreply.github.com>
2019-09-19 17:17:25 +0200

    module scene intro delete

--- AmazingEngine/Application.cpp
+++ AmazingEngine/Application.cpp
@@ -5,7 +5,6 @@ Application::Application()
 	window = new ModuleWindow(this);
 	input = new ModuleInput(this);
 	audio = new ModuleAudio(this, true);
-	scene_intro = new ModuleSceneIntro(this);
 	renderer3D = new ModuleRenderer3D(this);
 	camera = new ModuleCamera3D(this);
 	physics = new ModulePhysics3D(this);
@@ -20,9 +19,6 @@ Application::Application()
 	AddModule(input);
 	AddModule(audio);
 	AddModule(physics);
-	
-	// Scenes
-	AddModule(scene_intro);
 
 	// Renderer last!
 	AddModule(renderer3D);
