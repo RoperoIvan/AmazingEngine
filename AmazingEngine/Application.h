02383fc506fdd7c64e4651f0bf453f0e7c343ee5
didaclis <36189205+didaclis@users.noreply.github.com>
2019-09-21 19:53:06 +0200

    STL

--- AmazingEngine/Application.h
+++ AmazingEngine/Application.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "p2List.h"
 #include "Globals.h"
 #include "Timer.h"
 #include "Module.h"
@@ -9,6 +8,8 @@
 #include "ModuleRenderer3D.h"
 #include "ModuleCamera3D.h"
 #include "GuiManager.h"
+
+#include <list>
 
 class Application
 {
@@ -23,7 +24,7 @@ private:
 
 	Timer	ms_timer;
 	float	dt;
-	p2List<Module*> list_modules;
+	std::list<Module*> list_modules;
 
 public:
 
