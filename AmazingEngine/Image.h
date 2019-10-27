3064435c6fb7667c9c73bd91f4e5a751ee76811d
RoperoIvan <ivan_ropero98@hotmail.com>
2019-10-27 19:17:17 +0100

    Checker finally wooorks

--- AmazingEngine/Image.h
+++ AmazingEngine/Image.h
@@ -26,7 +26,9 @@ public:
 	void LoadCoords(par_shapes_mesh* p_mesh);
 	void LoadMaterials(const aiScene* scene, std::string file_name);
 	void LoadBuffers();
+	void LoadCheckerTexture();
 	int GetTextureId();
+	int SetTextureId(int id);
 	void ShowProperties() {};
 	std::string GetTexturePath();
 public:
@@ -34,7 +36,9 @@ public:
 	uint num_coords = 0;
 	float* uv_coord = nullptr;
 	int texture_id = 0;
+	int tmp_id = 0;
 	std::string p_tex;
+	uint check_id = 0;
 };
 
 #endif
